7f8360c53ac9ad08948539e5cf4e8cad576ce475
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-03-04 09:19:38 -0300

    adjusts

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 app.get('/usuarios', async (req, res) => {
   const users = await prisma.user.findMany()
